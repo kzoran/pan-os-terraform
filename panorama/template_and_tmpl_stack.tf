dae8cbf5b77e76c5a09482ef7ca0e237eb95987f
kzoran <zoransub+github@icloud.com>
2022-12-13 21:11:18 -0800

    updated template and stack

--- panorama/template_and_tmpl_stack.tf
+++ panorama/template_and_tmpl_stack.tf
@@ -2,26 +2,26 @@
 #######  Panorama template and template stack  #######
 
 resource "panos_panorama_template" "pa-vm-0_template" {
-  name = var.template
+  name        = var.template
   description = "template for pa-vm-0 firewalls"
   device {
-        serial = var.serial1
-    }
-    device {
-        serial = var.serial2
-    }
-    lifecycle {
-        create_before_destroy = true
-    }
+    serial = var.serial1
+  }
+  device {
+    serial = var.serial2
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "panos_panorama_template_stack" "pa-vm-0_demo_stack" {
-  name      = var.stack
+  name        = var.stack
   description = "template stack name for pa-vm-o firewalls"
-  templates = ["var.template"]
-  devices = ["var.serial1", " var.serial2"]
+  templates   = ["var.template"]
+  devices     = ["var.serial1", " var.serial2"]
 
   lifecycle {
-        create_before_destroy = true
-    }
+    create_before_destroy = true
+  }
 }
