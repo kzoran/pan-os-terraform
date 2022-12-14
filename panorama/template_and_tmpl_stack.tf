59925fd257b677013d55815d96a1394b6a70beaa
kzoran <zoransub+github@icloud.com>
2022-12-13 21:19:38 -0800

    fixed formatting

--- panorama/template_and_tmpl_stack.tf
+++ panorama/template_and_tmpl_stack.tf
@@ -3,8 +3,14 @@
 
 resource "panos_panorama_template" "pa-vm-0_template" {
   name        = var.template
-  description = "template for pa-vm-0 firewalls"
-  devices     = ["var.serial1", " var.serial2"]
+  description = "template for pa-vm-0 firewalls - created with terraform"
+  device { 
+    serial = var.serial1
+  }
+  device {
+    serial = var.serial2
+  }
+
   lifecycle {
     create_before_destroy = true
   }
@@ -12,7 +18,7 @@ resource "panos_panorama_template" "pa-vm-0_template" {
 
 resource "panos_panorama_template_stack" "pa-vm-0_demo_stack" {
   name        = var.stack
-  description = "template stack name for pa-vm-o firewalls"
+  description = "template stack name for pa-vm-o firewalls - created with terraform"
   templates   = ["var.template"]
   devices     = ["var.serial1", " var.serial2"]
 
