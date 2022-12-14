89148c588249e4f9406b4362c64878a5da0d98f1
kzoran <zoransub+github@icloud.com>
2022-12-14 14:29:20 -0800

    adding config for a virtual router

--- panorama/virtual_router.tf
+++ panorama/virtual_router.tf
@@ -1,6 +1,6 @@
 resource "panos_virtual_router" "VR1" {
     name = "VR1"
-    description = "Virtual router created with TF for PA-VM-0 template"
+    #description = "Virtual router created with TF for PA-VM-0 template"
     #static_dist = 15
     #interfaces = [
     #    panos_ethernet_interface.e1.name,
