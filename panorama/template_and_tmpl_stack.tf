e957add92fdc11b160e480f919b8a0ee8c68faa1
kzoran <zoransub+github@icloud.com>
2022-12-14 13:04:41 -0800

    updating panos_panorama_template

--- panorama/template_and_tmpl_stack.tf
+++ panorama/template_and_tmpl_stack.tf
@@ -9,9 +9,9 @@ resource "panos_panorama_template" "pa-vm-0_template" {
 
 
 
-#  device { 
-#    serial = var.serial1
-#  }
+  device { 
+    serial = var.serial1
+  }
 #  device {
 #    serial = var.serial2
 #  }
