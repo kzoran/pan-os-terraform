04c1fd44f3f1297d2929edca401b9e51864dd4b5
kzoran <zoransub+github@icloud.com>
2022-12-14 13:45:14 -0800

    updating panos_panorama_template_stack

--- panorama/template_and_tmpl_stack.tf
+++ panorama/template_and_tmpl_stack.tf
@@ -22,15 +22,15 @@ resource "panos_panorama_template" "pa-vm-0_template" {
 }
 
 
-resource "panos_panorama_template_entry" "pa-vm-0-template_entry" {
-    template = panos_panorama_template.pa-vm-0_template.name
-    serial = var.serial1
-    #serial = var.serial2
-
-    lifecycle {
-        create_before_destroy = true
-    }
-}
+#resource "panos_panorama_template_entry" "pa-vm-0-template_entry" {
+#    template = panos_panorama_template.pa-vm-0_template.name
+#    serial = var.serial1
+#    #serial = var.serial2
+#
+#    lifecycle {
+#        create_before_destroy = true
+#    }
+#}
 
 
 #resource "panos_panorama_template_entry" "example_tmplentry" {
@@ -40,13 +40,13 @@ resource "panos_panorama_template_entry" "pa-vm-0-template_entry" {
 #}
 #
 #
-#resource "panos_panorama_template_stack" "pa-vm-0_stack" {
-#  name        = var.stack
-#  description = "template stack name for pa-vm-o firewalls - created with terraform"
-#  templates   = ["var.template"]
-#  devices     = ["var.serial1", " var.serial2"]
-#
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
+resource "panos_panorama_template_stack" "pa-vm-0_stack" {
+  name        = var.stack
+  description = "template stack name for pa-vm-o firewalls - created with terraform"
+  templates   = ["var.template"]
+  devices     = ["var.serial1", " var.serial2"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
