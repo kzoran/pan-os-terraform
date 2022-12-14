e2bf6991eeb1c346e3591c91124218fa1238b28c
kzoran <zoransub+github@icloud.com>
2022-12-14 13:11:14 -0800

    updating panos_panorama_template

--- panorama/template_and_tmpl_stack.tf
+++ panorama/template_and_tmpl_stack.tf
@@ -25,7 +25,7 @@ resource "panos_panorama_template" "pa-vm-0_template" {
 resource "panos_panorama_template_entry" "example_tmplentry" {
 template = "${panos_panorama_template.pa-vm-0_template.name}"
 ##serial = "007254000068426"
-serial = "007054000062810"
+serial = var.serial1
 }
 #
 #
