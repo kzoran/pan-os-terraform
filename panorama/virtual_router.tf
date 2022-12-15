a630286f428090578f4cdcfd64340cac0d9c120f
kzoran <zoransub+github@icloud.com>
2022-12-15 15:39:21 -0800

    added HA interfaces

--- panorama/virtual_router.tf
+++ panorama/virtual_router.tf
@@ -1,3 +1,17 @@
+
+resource "panos_static_route_ipv4" "default_route_VR1" {
+    name = "localnet"
+    virtual_router = panos_virtual_router.VR1.name
+    destination = "0.0.0.0/0"
+    next_hop = "172.21.31.1"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+
+
 resource "panos_virtual_router" "VR1" {
   name = var.vr_name
   #description = "Virtual router created with TF for PA-VM-0 template"
@@ -14,14 +28,3 @@ resource "panos_virtual_router" "VR1" {
   }
 }
 
-
-resource "panos_static_route_ipv4" "default_route_VR1" {
-    name = "localnet"
-    virtual_router = panos_virtual_router.VR1.name
-    destination = "0.0.0.0/0"
-    next_hop = "172.21.31.1"
-
-    lifecycle {
-        create_before_destroy = true
-    }
-}
