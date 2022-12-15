d73dc03c5a3b7362f3a63004ec8a5fde66bf136e
kzoran <zoransub+github@icloud.com>
2022-12-15 14:58:13 -0800

    added HA interfaces

--- panorama/terraform.tfvars
+++ panorama/terraform.tfvars
@@ -29,9 +29,6 @@ eth1_name = "ethernet1/1"
 eth1_mode = "layer3"
 eth1_vsys = "vsys1"
 eth1_comment = "WAN Interface"
-eth1_dhcp          = true
-eth1_default_route = true
-eth1_dhcp_send_hostname_enable = true
 
 
 # Eth2 Interface
@@ -44,25 +41,17 @@ eth2_ip = "192.168.55.10/24"
 eth3_name = "ethernet1/3"
 eth3_mode = "layer3"
 eth3_vsys = "vsys1"
-eth3_dhcp          = true
-eth3_default_route = true
-eth3_dhcp_send_hostname_enable = true
 
 # Eth5 Interface
 eth4_name = "ethernet1/4"
 eth4_mode = "layer3"
 eth4_vsys = "vsys1"
-eth4_dhcp          = true
-eth4_default_route = true
-eth4_dhcp_send_hostname_enable = true
+
 
 # Eth5 Interface
 eth5_name = "ethernet1/5"
 eth5_mode = "layer3"
 eth5_vsys = "vsys1"
-eth5_dhcp          = true
-eth5_default_route = true
-eth5_dhcp_send_hostname_enable = true
 
 
 # HA-1 Interface
@@ -79,7 +68,3 @@ eth9_comment = "HA2 Interface"
 
 
 
-l3_mode = "layer3"
-l2_mode = "layer2"
-ha_mode = "ha"
-vsys1 = "vsys1"
--- panorama/variables.tf
+++ panorama/variables.tf
@@ -35,6 +35,123 @@ variable "serial1" {
 variable "serial2" {
   description = "firewall 2  serial number"
 }
+#######################################################
+###############Ethernet Interfaces ####################
+
+variable "eth1_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth1_mode" {
+  description = "Interface mode"
+}
+
+variable "eth1_vsys" {
+  description = "eth1 vsys assignment"
+}
+
+variable "eth1_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+
+variable "eth1_default_route" {
+  description = "Accept default route"
+}
+
+variable "eth2_name" {
+  description = "Name of our Ethernet 1/2 interface"
+}
+
+variable "eth2_mode" {
+  description = "Mode of our Ethernet 1/2 interface"
+}
+
+variable "eth2_vsys" {
+  description = "eth2 vsys assignment"
+}
+
+variable "eth2_ips" {
+  description = "Static IP assignment"
+}
+
+variable "eth3_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth3_mode" {
+  description = "Interface mode"
+}
+
+variable "eth3_vsys" {
+  description = "eth3 vsys assignment"
+}
+
+variable "eth3_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+variable "eth4_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth4_mode" {
+  description = "Interface mode"
+}
+
+variable "eth4_vsys" {
+  description = "eth4 vsys assignment"
+}
+
+variable "eth4_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+variable "eth5_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth5_mode" {
+  description = "Interface mode"
+}
+
+variable "eth5_vsys" {
+  description = "eth5 vsys assignment"
+}
+variable "eth8_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth8_mode" {
+  description = "Interface mode"
+}
+
+variable "eth8_vsys" {
+  description = "eth8 vsys assignment"
+}
+
+variable "eth8_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+
+
+variable "eth5_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+
+variable "eth9_name" {
+  description = "Name of our Ethernet 1/1 interface"
+}
+
+variable "eth9_mode" {
+  description = "Interface mode"
+}
+
+variable "eth9_vsys" {
+  description = "eth9 vsys assignment"
+}
+
+variable "eth9_dhcp" {
+  description = "Enable interface for DHCP client"
+}
+
 
 #variable "interface" {
 #  description = "network interfaces"
