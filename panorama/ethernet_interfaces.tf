c1765dab4f200685bbac705f48c5bd3609391818
kzoran <zoransub+github@icloud.com>
2022-12-14 16:33:28 -0800

    adding config for a virtual router

--- panorama/ethernet_interfaces.tf
+++ panorama/ethernet_interfaces.tf
@@ -1,13 +1,13 @@
-resource "panos_panorama_ethernet_interface" "WAN" {
-    name                      = "ethernet1/1"
-    comment                   = "WAN interface"
-    vsys                      = "vsys1"
-    mode                      = "layer3"
-    enable_dhcp               = true
-    create_dhcp_default_route = true
-    #template                  = panos_panorama_template.demo_template.name
-    template                  = var.template
-}
+#resource "panos_panorama_ethernet_interface" "WAN" {
+#    name                      = "ethernet1/1"
+#    comment                   = "WAN interface"
+#    vsys                      = "vsys1"
+#    mode                      = "layer3"
+#    enable_dhcp               = true
+#    create_dhcp_default_route = true
+#    #template                  = panos_panorama_template.demo_template.name
+#    template                  = var.template
+#}
 #resource "panos_panorama_ethernet_interface" "web" {
 #    name        = "ethernet1/2"
 #    comment     = "web interface"
