851d8bf6d5be2688ba96799860f10a95309d756b
kzoran <zoransub+github@icloud.com>
2022-12-15 14:40:14 -0800

    added HA interfaces

--- panorama/ethernet_interfaces.tf
+++ panorama/ethernet_interfaces.tf
@@ -1,14 +1,25 @@
 resource "panos_panorama_ethernet_interface" "wan" {
-    name                      = "ethernet1/1"
-    comment                   = "WAN interface"
-    vsys                      = "vsys1"
-    mode                      = "layer3"
+    name                      = var.eth1_name
+    comment                   = var.eth1_comment
+    vsys                      = var.eth1_vsys
+    mode                      = var.eth1_mode
     enable_dhcp               = true
     create_dhcp_default_route = true
     dhcp_send_hostname_enable = true
-    #template                  = panos_panorama_template.demo_template.name
     template                  = var.template
 }
+
+resource "panos_panorama_ethernet_interface" "eth2" {
+    name                      = var.eth2_name
+    comment                   = var.eth2_comment
+    vsys                      = var.eth2_vsys
+    mode                      = var.eth2_mode
+    enable_dhcp               = true
+    create_dhcp_default_route = true
+    dhcp_send_hostname_enable = true
+    template                  = var.template
+}
+
 #resource "panos_panorama_ethernet_interface" "web" {
 #    name        = "ethernet1/2"
 #    comment     = "web interface"
@@ -28,20 +39,20 @@ resource "panos_panorama_ethernet_interface" "wan" {
 #}
 
 resource "panos_panorama_ethernet_interface" "ha-1" {
-    name                      = "ethernet1/8"
-    comment                   = "HA1 interface"
-    vsys                      = "vsys1"
-    mode                      = "ha"
+    name                      = var.eth8_name
+    comment                   = var.eth8_comment
+    vsys                      = var.eth8_vsys
+    mode                      = var.eth8_mode
 #    enable_dhcp               = true
 #    create_dhcp_default_route = true
     #template                  = panos_panorama_template.demo_template.name
    template                  = var.template
 }
 resource "panos_panorama_ethernet_interface" "ha-2" {
-    name                      = "ethernet1/9"
-    comment                   = "HA2 interface"
-    vsys                      = "vsys1"
-    mode                      = "ha"
+    name                      = var.eth9_name
+    comment                   = var.eth9_comment
+    vsys                      = var.eth9_vsys
+    mode                      = var.eth9_mode
 #    enable_dhcp               = true
 #    create_dhcp_default_route = true
     #template                  = panos_panorama_template.demo_template.name
--- panorama/terraform.tfvars
+++ panorama/terraform.tfvars
@@ -18,3 +18,68 @@ stack        = "PA-VM-0-stack"
 
 serial2 = "007951000346611"
 serial1 = "007951000346600"
+
+
+################################################
+######### Firewall Network Interfaces  #########
+######## Comment out unused interfaces  ########
+
+# WAN Interface
+eth1_name = "ethernet1/1"
+eth1_mode = "layer3"
+eth1_vsys = "vsys1"
+eth1_comment = "WAN Interface"
+eth1_dhcp          = true
+eth1_default_route = true
+eth1_dhcp_send_hostname_enable = true
+
+
+# Eth2 Interface
+eth2_name = "ethernet1/2"
+eth2_mode = "layer3"
+eth2_vsys = "vsys1"
+eth2_ip = "192.168.55.10/24"
+
+# Eth3 Interface
+eth3_name = "ethernet1/3"
+eth3_mode = "layer3"
+eth3_vsys = "vsys1"
+eth3_dhcp          = true
+eth3_default_route = true
+eth3_dhcp_send_hostname_enable = true
+
+# Eth5 Interface
+eth4_name = "ethernet1/4"
+eth4_mode = "layer3"
+eth4_vsys = "vsys1"
+eth4_dhcp          = true
+eth4_default_route = true
+eth4_dhcp_send_hostname_enable = true
+
+# Eth5 Interface
+eth5_name = "ethernet1/5"
+eth5_mode = "layer3"
+eth5_vsys = "vsys1"
+eth5_dhcp          = true
+eth5_default_route = true
+eth5_dhcp_send_hostname_enable = true
+
+
+# HA-1 Interface
+eth8_name = "ethernet1/8"
+eth8_mode = "ha"
+eth8_vsys = "vsys1"
+eth8_comment = "HA1 Interface"
+
+# HA-2 Interface
+eth8_name = "ethernet1/9"
+eth8_mode = "ha"
+eth8_vsys = "vsys1"
+eth8_comment = "HA2 Interface"
+
+
+
+l3_mode = "layer3"
+l2_mode = "layer2"
+ha_mode = "ha"
+vsys1 = "vsys1"
