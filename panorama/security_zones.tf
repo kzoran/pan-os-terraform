a65cf6181d2564528a7924c2a5edd9a714527f88
kzoran <zoransub+github@icloud.com>
2022-12-14 16:50:44 -0800

    created security zone policy

--- panorama/security_zones.tf
+++ panorama/security_zones.tf
@@ -1,7 +1,7 @@
 ##############################################################
 ####################### Security zones    ####################
 resource "panos_panorama_zone" "wan" {
-  name       = "WAN"
+  name       = "wan"
   mode       = "layer3"
   interfaces = [panos_panorama_ethernet_interface.wan.name]
   template   = panos_panorama_template.pa-vm-0_template.name
