584c265400d3688a227e388c21e29e64be71fbb6
kzoran <zoransub+github@icloud.com>
2022-12-15 15:42:41 -0800

    adding static route

--- panorama/virtual_router.tf
+++ panorama/virtual_router.tf
@@ -1,16 +1,16 @@
 
-resource "panos_static_route_ipv4" "default_route_VR1" {
-    name = "localnet"
-    virtual_router = panos_virtual_router.VR1.name
-    destination = "0.0.0.0/0"
-    next_hop = "172.21.31.1"
-
-    lifecycle {
-        create_before_destroy = true
-    }
-}
-
-
+#resource "panos_static_route_ipv4" "default_route_VR1" {
+#    name = "localnet"
+#    virtual_router = panos_virtual_router.VR1.name
+#    destination = "0.0.0.0/0"
+#    next_hop = "172.21.31.1"
+#
+#    lifecycle {
+#        create_before_destroy = true
+#    }
+#}
+#
+#
 
 resource "panos_virtual_router" "VR1" {
   name = var.vr_name
