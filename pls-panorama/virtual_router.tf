eaafa435361956b388902dee5b7bc5d0a4c27ed4
kzoran <zoransub+github@icloud.com>
2022-12-20 16:28:18 -0800

    starting with PLS Lab deployment

--- pls-panorama/virtual_router.tf
+++ pls-panorama/virtual_router.tf
@@ -17,7 +17,7 @@ resource "panos_virtual_router" "VR1" {
   #description = "Virtual router created with TF for PA-VM-0 template"
   #static_dist = 15
   interfaces = [
-    panos_panorama_ethernet_interface.wan.name
+    panos_panorama_ethernet_interface.wan.name,
     panos_panorama_ethernet_interface.Users.name,
     panos_panorama_ethernet_interface.DAAS.name,
     panos_panorama_ethernet_interface.CriticalInfrastructure.name,
